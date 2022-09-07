8474411bf435b50f0ab49e235de0e250e09a7537
groupe P <unilugroupep@gmail.com>
2022-09-07 05:26:20 +0200

    tp pour le tiroir

---
+++ app/src/main/java/com/example/tiroir/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.tiroir;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final DrawerLayout drawerLayout = findViewById(R.id.accuiel);
+
+        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        NavigationView navigationView = findViewById(R.id.navue);
+        NavController navController = Navigation.findNavController(this, R.id.frag);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+}
